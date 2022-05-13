289c1a53e62acbe10464587d13cab4773d32cd56
Fernando Galvan <n.brighter00@gmail.com>
2022-05-12 20:22:05 -0500

    added title

--- giphyreact/src/components/giphycontainer/Container.jsx
+++ giphyreact/src/components/giphycontainer/Container.jsx
@@ -5,6 +5,7 @@ export default function Container(props) {
     if (props.giphy){
           return <div>
             <img src={props.giphy.data.images.original.url} alt={props.giphy.id}/>
+            <h3>{props.giphy.data.title}</h3>
           </div>
     } else{
         return <h1>Search!</h1>
