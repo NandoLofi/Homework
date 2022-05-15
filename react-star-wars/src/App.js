c17053cb5f6c83bdc596be679356257ffd3d4c83
Fernando Galvan <n.brighter00@gmail.com>
2022-05-14 19:01:02 -0500

    setting url fetch

--- react-star-wars/src/App.js
+++ react-star-wars/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+  //set state
+  const [state, setState] = useState(null)
+  //fetch URL
+  const getShip = async(searchTerm)=>{
+  const response = await fetch("https://swapi.dev/api/starship")
+  const data = await response.json()
+  setState(data)
+  }
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fleet of Ships</h1>
+    
+
+
     </div>
   );
 }
