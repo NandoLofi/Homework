dc1225acc5f48a63dfb353390c5589877cbc2914
Fernando Galvan <n.brighter00@gmail.com>
2022-05-16 15:57:00 -0500

    setting up nesting the ships object

--- react-star-wars/src/App.js
+++ react-star-wars/src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 import {useState, useEffect} from 'react'
 
 function App() {
-  //set state
-  const [state, setState] = useState(null)
-  //fetch URL
-  const getShip = async(searchTerm)=>{
-  const response = await fetch("https://swapi.dev/api/starship")
-  const data = await response.json()
-  setState(data)
-  }
- 
+const [ship, setShip] = useState(null)
+
+const shipData = useEffect(()=>{
+  fetch("https://swapi.dev/api/starships")
+  .then(response => response.json())
+  .then(json => setShip(json))
+}, [ship])
+const allShips = shipData.map((ele, index)=>{
+
+
+})
 
   return (
     <div className="App">
-      <h1>Fleet of Ships</h1>
-    
-
+      <h1>Fleet of Ships</h1>    
 
     </div>
   );
