ed4ac9828e7fa7b8451bb6c1951649600ec016ea
Fernando Galvan <n.brighter00@gmail.com>
2022-05-16 20:31:02 -0500

    rewriting code for useeffect

--- react-star-wars/src/App.js
+++ react-star-wars/src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 import {useState, useEffect} from 'react'
 
 function App() {
-const [ship, setShip] = useState(null)
+const [ships, setShips] = useState([])
+const allShips = async()=>{
+  const response = await fetch("https://swapi.dev/api/starships/")
+  const data = response.json()
+  setShips(data)
+  console.log(data)
+}
 
-const shipData = useEffect(()=>{
-  fetch("https://swapi.dev/api/starships")
-  .then(response => response.json())
-  .then(json => setShip(json))
-}, [ship])
-const allShips = shipData.map((ele, index)=>{
 
+useEffect(()=>{
+}, [])
 
-})
 
   return (
     <div className="App">
       <h1>Fleet of Ships</h1>    
-
     </div>
   );
 }
