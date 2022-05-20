b3173decf87cd07ce5ea6eef4e40dee0f984caf9
Fernando Galvan <n.brighter00@gmail.com>
2022-05-20 14:20:29 -0500

    first commit after setup

---
+++ stocksrouter/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Route, BrowserRouter as Router} from 'react'
+
+function App() {
+  return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default App;
