33bd250098e6f60524c147b1a75a07c06583b4a8
Fernando Galvan <n.brighter00@gmail.com>
2022-05-25 10:43:07 -0500

    added nav and components

---
+++ stocksrouter/src/Components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Nav() {
+  return (
+    <div className='nav'>
+        <Link to="/">
+            <div>Home Page</div>
+        </Link>
+        <Link to="/stocks">
+            <div>All Stocks</div>
+        </Link>
+    </div>
+  )
+}
